fe7ed821e6c382cb15c42940d8c9c4c3cb5b2c17
Nguyen <nguyenphambaonguyen@gmail.com>
2023-02-09 16:14:46 +0700

    Enable CORS

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 4000;
 
 const getStockPrice = (range, base) =>
