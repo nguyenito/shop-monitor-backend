1bd41bec21a17bf9174ab09d1533ac705e207793
Nguyen <nguyenphambaonguyen@gmail.com>
2023-02-09 15:13:07 +0700

    Print welcome message

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Welcom to Shop Monitor Service!');
+});
 app.listen(port, function () {
   console.log(`Server is running on port ${port}`);
 });
