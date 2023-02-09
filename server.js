783bb6d5df496abbd9fe77a11af5b29ffe9e84e6
Nguyen <nguyenphambaonguyen@gmail.com>
2023-02-09 15:46:18 +0700

    Add sse for simulate stock price

--- server.js
+++ server.js
@@ -3,6 +3,27 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 
+const getStockPrice = (range, base) =>
+  (Math.random() * range + base).toFixed(2);
+const getTime = () => new Date().toLocaleTimeString();
+
+app.post('/stream', function (req, res) {
+  res.writeHead(200, {
+    Connection: 'keep-alive',
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+  });
+  setInterval(() => {
+    res.write(
+      `data: {"time": "${getTime()}", "aTechStockPrice": "${getStockPrice(
+        2,
+        20
+      )}", "bTechStockPrice": "${getStockPrice(4, 22)}"}`
+    );
+    res.write('\n\n');
+  }, 5000);
+});
+
 app.get('/', (req, res) => {
   res.send('Welcom to Shop Monitor Service!');
 });
