f7ca32bc241409c776b114077afda40dcba5bea9
Nguyen <nguyenphambaonguyen@gmail.com>
2023-02-12 20:40:10 +0700

    Finalize for server to tracking

--- monitor.js
+++ monitor.js
@@ -1,113 +1,33 @@
-// const puppeteer = require('puppeteer');
-// const fs = require('fs');
-// const { CronJob } = require('cron');
-// const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
 
-// // const rand_url = 'https://scraping.official.ec/items/40792454';//Out of Stock
-// const rand_url = 'https://scraping.official.ec/items/40792284'; //In Stock
+async function sendNotification(productName, productLink, recvEmail) {
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'npbnguyenbot@gmail.com',
+      pass: 'zbpugjpwgqrwhnsk',
+    },
+  });
 
-// async function initBrowser() {
-//   const browser = await puppeteer.launch({ headless: true });
-//   const page = await browser.newPage();
-//   return page;
-// }
+  let textToSend =
+    'Dear Friend, \n\nAs you might know I been monitoring this product day and night for you.\nIT IS IN STOCK NOW.\nSo do not miss this chance. Go and get it now!\n';
+  let htmlText = 'Product Link: ' + productLink;
+  textToSend = textToSend + htmlText;
 
-// async function checkStock(page) {
-//   await page.goto(rand_url);
-//   let content = await page.evaluate(() => document.body.innerHTML);
+  let mailOptions = {
+    from: 'Online Shop Monitor <npbnguyenbot@gmail.com>',
+    to: recvEmail,
+    subject: `PRODUCT [${productName}] IS IN STOCK`,
+    text: textToSend,
+  };
 
-//   let isSoldOut = checkUdemyExample(content);
-//   if (isSoldOut) console.log('Out Of Stock');
-//   else {
-//     console.log('Product In Stock');
-//     // await sendNotification(rand_url, 'nguyenphambaonguyen@gmail.com');
-//   }
-
-//   console.log('Done');
-// }
-
-// function checkUdemyExample(webContent) {
-//   const itemDetailPattern = /class="(item-detail_.*?)"/g;
-//   const soldOutPattern = /soldout/gi;
-//   const itemMatch = [...webContent.matchAll(itemDetailPattern)];
-
-//   let isSoldOut = false;
-//   if (itemMatch != null) {
-//     for (let i = 0; i < itemMatch.length; i++) {
-//       matchContent = itemMatch[i][1];
-//       if (soldOutPattern.test(matchContent)) {
-//         isSoldOut = true;
-//         break;
-//       }
-//     }
-//   }
-//   return isSoldOut;
-// }
-
-// async function monitor() {
-//   console.log('Init Browser and Go the web page');
-//   let page = await initBrowser();
-
-//   let job = new CronJob(
-//     '*/1 * * * *',
-//     function () {
-//       console.log('Checking stock...');
-//       checkStock(page);
-//     },
-//     null,
-//     true,
-//     null,
-//     null,
-//     true
-//   );
-//   job.start();
-// }
-
-// async function sendNotification(web_url, recvEmail) {
-//   let transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//       user: 'npbnguyenbot@gmail.com',
-//       pass: 'zbpugjpwgqrwhnsk',
-//     },
-//   });
-
-//   let textToSend =
-//     'Dear Friend, \n\nAs you might know I been monitoring this product day and night for you.\nIT IS IN STOCK NOW.\nSo do not miss this chance. Go and get it now!\n';
-//   let htmlText = 'Product Link: ' + web_url;
-//   textToSend = textToSend + htmlText;
-
-//   let mailOptions = {
-//     from: 'Online Shop Monitor <npbnguyenbot@gmail.com>',
-//     to: recvEmail,
-//     subject: 'UDEMY QQQ PRODUCT IS IN STOCK',
-//     text: textToSend,
-//   };
-
-//   transporter.sendMail(mailOptions, function (error, info) {
-//     if (error) {
-//       console.log(error);
-//     } else {
-//       console.log('Email sent: ' + info.response);
-//     }
-//   });
-// }
-
-// // sendNotification(rand_url, 'nguyenphambaonguyen@gmail.com');
-
-async function monitorTest() {
-  console.log('Init Browser and Go the web page');
-
-  setInterval(() => {
-    console.log('Checking stock...');
-    const randNumber = Math.floor(Math.random() * 2);
-    if (randNumber == 0) {
-      const productQuantity = Math.floor(Math.random() * 100) + 1;
-      console.log(`Product In Stock. Product Quantity ${productQuantity}`);
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
     } else {
-      console.log('Product Out of Stock');
+      console.log('Email sent: ' + info.response);
     }
-  }, 5000);
+  });
 }
 
-module.exports = { monitorTest };
+module.exports = { sendNotification };
--- server.js
+++ server.js
@@ -1,34 +1,197 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const puppeteer = require('puppeteer');
+const { sendNotification } = require('./monitor.js');
+const axios = require('axios');
+
+const port = process.env.PORT || 4000;
 
 const app = express();
 app.use(cors());
-const port = process.env.PORT || 4000;
-
-const getStockPrice = (range, base) =>
-  (Math.random() * range + base).toFixed(2);
-const getTime = () => new Date().toLocaleTimeString();
-
-app.post('/stream', function (req, res) {
-  res.writeHead(200, {
-    Connection: 'keep-alive',
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-  });
-  setInterval(() => {
-    res.write(
-      `data: {"time": "${getTime()}", "aTechStockPrice": "${getStockPrice(
-        2,
-        20
-      )}", "bTechStockPrice": "${getStockPrice(4, 22)}"}`
-    );
-    res.write('\n\n');
-  }, 5000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.listen(port, function () {
+  console.log(`Server is running on port ${port}`);
 });
 
 app.get('/', (req, res) => {
   res.send('Welcom to Shop Monitor Service!');
 });
-app.listen(port, function () {
-  console.log(`Server is running on port ${port}`);
-});
+
+const MONITOR_INTERVAL_SECONDS = 60;
+const JSON_SERVER = 'http://localhost:3001';
+const DATABASE_URL = `${JSON_SERVER}`;
+const PRODUCTS_DB_URL = `${DATABASE_URL}/products`;
+
+clients = [];
+
+async function fetchJSonData(url) {
+  return axios
+    .get(url)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+      return [];
+    });
+}
+
+async function updateProductDataToDb(product) {
+  let url = `${PRODUCTS_DB_URL}/${product.id}`;
+  return axios.put(url, product).catch((error) => {
+    console.log(error);
+    return;
+  });
+}
+
+async function fetchNotificationInfo() {
+  let url = `${DATABASE_URL}/notification_info/1`;
+  return fetchJSonData(url);
+}
+
+async function updateAlarmCountDataToDb(alarmCount) {
+  let url = `${DATABASE_URL}/notification_info/1`;
+  return axios.patch(url, { alarm_count: alarmCount }).catch((error) => {
+    console.log(error);
+    return;
+  });
+}
+
+async function sendProductDataToAll(product, event_type) {
+  product['event_type'] = event_type;
+  const data = `data: ${JSON.stringify(product)}\n\n`;
+  for (let i = 0; i < clients.length; ++i) {
+    clients[i].response.write(data);
+  }
+}
+
+function sendEventData(response, products_data, event_type) {
+  for (let i = 0; i < products_data.length; ++i) {
+    products_data[i]['event_type'] = event_type;
+    const data = `data: ${JSON.stringify(products_data[i])}\n\n`;
+    response.write(data);
+  }
+}
+
+async function eventsHandler(res, response, next) {
+  const headers = {
+    'Content-Type': 'text/event-stream',
+    Connection: 'keep-alive',
+    'Cache-Control': 'no-cache',
+  };
+  response.writeHead(200, headers);
+
+  const productsData = await fetchJSonData(PRODUCTS_DB_URL);
+  sendEventData(response, productsData, 'information');
+
+  const clientId = Date.now();
+  const newClient = {
+    id: clientId,
+    response,
+  };
+  clients.push(newClient);
+
+  res.on('close', () => {
+    console.log(`${clientId} Connection closed`);
+    clients = clients.filter((client) => client.id !== clientId);
+  });
+}
+app.get('/events', eventsHandler);
+
+async function initBrowser() {
+  const browser = await puppeteer.launch({ headless: true });
+  return browser;
+}
+
+function checkUdemyExample(webContent) {
+  const itemDetailPattern = /class="(item-detail_.*?)"/g;
+  const soldOutPattern = /soldout/gi;
+  const itemMatch = [...webContent.matchAll(itemDetailPattern)];
+
+  let isSoldOut = false;
+  if (itemMatch != null) {
+    for (let i = 0; i < itemMatch.length; i++) {
+      matchContent = itemMatch[i][1];
+      if (soldOutPattern.test(matchContent)) {
+        isSoldOut = true;
+        break;
+      }
+    }
+  }
+  return isSoldOut;
+}
+
+let pages = [];
+async function checkProductStock(page, product) {
+  await page.goto(product.product_url, { timeout: 0 });
+
+  let evaluateDocument = null;
+  while (evaluateDocument === null) {
+    evaluateDocument = await page.evaluate(() => {
+      if (document != null) return document.body.innerHTML;
+      else return null;
+    });
+  }
+
+  const content = evaluateDocument;
+  let isSoldOut = checkUdemyExample(content);
+  let newStatus = 'Unknown';
+
+  if (isSoldOut) {
+    newStatus = 'OutStock';
+  } else {
+    newStatus = 'InStock';
+  }
+
+  let isSendAlarm = false;
+  if (product.status !== newStatus) {
+    console.log(`Product id#${product.id} State Changed. ALARM!!!`);
+    product.status = newStatus;
+    console.log(product);
+
+    sendNotification(
+      product.product_name,
+      product.product_url,
+      'nguyenphambaonguyen@gmail.com'
+    );
+    updateProductDataToDb(product);
+    isSendAlarm = true;
+  }
+  sendProductDataToAll(product, 'information');
+  if (isSendAlarm) {
+    const notificationData = await fetchNotificationInfo();
+    updateAlarmCountDataToDb(notificationData['alarm_count'] + 1);
+    sendProductDataToAll(product, 'alarm');
+  }
+}
+
+async function trackingProductsStock(browser) {
+  console.log('Tracking Products In Stock...');
+  const products_data = await fetchJSonData(PRODUCTS_DB_URL);
+  if (products_data.length > pages.length) {
+    const noOfNewPage = products_data.length - pages.length;
+    for (let i = 0; i < noOfNewPage; ++i) {
+      const page = await browser.newPage();
+      pages.push(page);
+    }
+  }
+  console.log(`Number of Chromium Page is Running: ${pages.length}`);
+  for (let i = 0; i < products_data.length; ++i) {
+    const checkingProduct = Object.assign({}, products_data[i]);
+    checkingProduct.status = 'Checking';
+    await sendProductDataToAll(checkingProduct, 'information');
+    checkProductStock(pages[i], products_data[i]);
+  }
+}
+
+async function startWatcher() {
+  console.log('Init Browser');
+  const browser = await initBrowser();
+
+  await trackingProductsStock(browser);
+  setInterval(trackingProductsStock, MONITOR_INTERVAL_SECONDS * 1000, browser);
+}
+
+startWatcher();
