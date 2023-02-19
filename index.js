076fbfa1a6627357f83afb5659b2326fa7af4726
Nguyen <nguyenphambaonguyen@gmail.com>
2023-02-19 14:56:58 +0700

    Test deploy server

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const puppeteer = require('puppeteer');
+const { sendNotification } = require('./monitor.js');
+const axios = require('axios');
+const { logEvents, logWebContent } = require('./log_events');
+const { checkProductInStock } = require('./product_pattern');
+require('dotenv').config();
+
+const port = process.env.PORT || 4000;
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.listen(port, function () {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcom to Shop Monitor Service!');
+});
